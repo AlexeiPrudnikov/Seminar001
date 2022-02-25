42f94de0af5525a45f8862fedc71bbf4093a7fe9
Alexei <alexei.prudnikov@gmail.com>
2022-02-25 21:14:37 +0300

    Add Task6

---
+++ Project03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("=======Задача № 6=======");
+Console.WriteLine("ННапишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+try
+{
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("n = " + n + " -> ");
+    if ((n % 2) == 0)
+    {
+        Console.WriteLine ("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
